c3b6fe1c97425ac29957b785c103ae884fc3b0fc
vijoin <>
2022-02-26 23:57:31 +0000

    Raise alert with emitted event containg the URL in opensea with the new NFt

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
 
+const CONTRACT_ADDRESS = "0xeE8cf816D93073eBbD948b5A66f9aaa68d729Ff9";
+
 const App = () => {
 
   const [currentAccount, setCurrentAccount] = useState("");
@@ -30,6 +32,7 @@ const App = () => {
       const account = accounts[0];
       console.log("Found an authorized account:", account);
       setCurrentAccount(account)
+      setupEventListener();
     } else {
       console.log("No authorized account found")
     }
@@ -48,13 +51,40 @@ const App = () => {
 
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
+      setupEventListener();
     } catch (error) {
       console.log(error)
     }
   }
 
+  const setupEventListener = async () => {
+    try {
+      const { ethereum } = window;
+      
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+        connectedContract.on(
+          "NewEpicNFTMinted", (from, tokenId) => {
+            console.log(from, tokenId.toNumber());
+            alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`);
+
+            console.log("Setup event listener!");
+
+          }
+        )
+        
+      } else {
+        console.log("Ethereum object doens't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   const askContractToMintNft = async () => {
-    const CONTRACT_ADDRESS = "0x1aeb4508c336351780d9eac597bd654a09798a7e";
 
     try {
       const { ethereum } = window;
